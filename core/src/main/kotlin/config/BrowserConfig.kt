b8e1ad3119cf2b1ef7047783e430e83e40a0557c
chibisov <chibisof81@yandex.ru>
2026-03-02 11:41:00 +0300

    init

--- core/src/main/kotlin/config/BrowserConfig.kt
+++ core/src/main/kotlin/config/BrowserConfig.kt
@@ -1,7 +1,6 @@
 package config
 
 import com.codeborne.selenide.Configuration
-import com.codeborne.selenide.Selenide
 import com.codeborne.selenide.logevents.SelenideLogger
 import io.qameta.allure.selenide.AllureSelenide
 import org.openqa.selenium.PageLoadStrategy
